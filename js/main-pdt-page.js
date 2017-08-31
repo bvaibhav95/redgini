43eb45142b50e1b05d7694df62da534d7ef24685
dev <saurabhrsikchi@gmail.com>
2017-08-31 21:28:33 +0530

    cake_pdt changes

--- js/main-pdt-page.js
+++ js/main-pdt-page.js
@@ -3,34 +3,34 @@ jQuery(document).ready(function(){
     $('.qtyplus').click(function(e){
         // Stop acting like a button
         e.preventDefault();
-        // Get the field name
-        fieldName = $(this).attr('field');
+        // Get the input element
+        input_element = $(this).parent().parent().find('input.qty');
         // Get its current value
-        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        var currentVal = parseInt(input_element.val());
         // If is not undefined
         if (!isNaN(currentVal)) {
             // Increment
-            $('input[name='+fieldName+']').val(currentVal + 1);
+            input_element.val(currentVal + 1);
         } else {
-            // Otherwise put a 0 there
-            $('input[name='+fieldName+']').val(0);
+            // Otherwise put a 1 there
+            input_element.val(1);
         }
     });
     // This button will decrement the value till 0
     $(".qtyminus").click(function(e) {
         // Stop acting like a button
         e.preventDefault();
-        // Get the field name
-        fieldName = $(this).attr('field');
+        // Get the input element
+        input_element = $(this).parent().parent().find('input.qty');
         // Get its current value
-        var currentVal = parseInt($('input[name='+fieldName+']').val());
-        // If it isn't undefined or its greater than 0
-        if (!isNaN(currentVal) && currentVal > 0) {
+        var currentVal = parseInt(input_element.val());
+        // If it isn't undefined or its greater than 1
+        if (!isNaN(currentVal) && currentVal > 1) {
             // Decrement one
-            $('input[name='+fieldName+']').val(currentVal - 1);
+            input_element.val(currentVal - 1);
         } else {
-            // Otherwise put a 0 there
-            $('input[name='+fieldName+']').val(0);
+            // Otherwise put a 1 there
+            input_element.val(1);
         }
     });
 });
